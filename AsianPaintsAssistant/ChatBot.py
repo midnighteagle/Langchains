aff4d6638e257a26766ccfddfab39d00b539d2a4
midnighteagle <chiranjeevivaston@gmail.com>
2026-09-11 23:57:58 +0530

    changes is done?

--- AsianPaintsAssistant/ChatBot.py
+++ AsianPaintsAssistant/ChatBot.py
@@ -557,7 +557,7 @@ Please contact the customer regarding this enquiry.
         # =================================================
         if not st.session_state.get("chatbot_started"):
             st.switch_page(
-                "pages/ChatBot.py"
+                "ChatBot.py"
             )
 
 
