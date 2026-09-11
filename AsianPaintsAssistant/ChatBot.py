f56c7996a759bf2e1265c11cd24df8885df536b2
midnighteagle <chiranjeevivaston@gmail.com>
2026-09-11 23:54:21 +0530

    i have changes something in it

--- AsianPaintsAssistant/ChatBot.py
+++ AsianPaintsAssistant/ChatBot.py
@@ -557,7 +557,7 @@ Please contact the customer regarding this enquiry.
         # =================================================
         if not st.session_state.get("chatbot_started"):
             st.switch_page(
-                "pages/chatbot1.py"
+                "pages/ChatBot.py"
             )
 
 
